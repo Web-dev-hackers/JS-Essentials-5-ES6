12b98c2fc167ebc56137b71e7fe3abaf3c0fe0d4
GhalyaAbdallah666 <ghalyaalathram@gmail.com>
2022-01-12 17:48:24 +0200

    this be confusing ngl

--- tasks.js
+++ tasks.js
@@ -1,30 +1,39 @@
 console.log("tasks file is linked, happy coding");
 //using ES6 syntax
 //0.a create a variable called 'myName' & give it your name
+let myName = "Ghalya";
 //0.b create an array called 'myFriends' give it at least 5 friends
+let myFriends = ["Bushra", "Nehal", "Aya", "Rae", "Lilo"];
 //0.c create an object called 'person' has keys (name: myName, friends: myFriends)
-
+const person = {name: myName, friends: myFriends};
 //1 get the value of 'name' from person object & using Destructing Assignment
-
+const {name, friends} = person;
 //2 get the second value of 'myFriends' array & assign it to 'bestee' using Destructing Assignment
-
+let [, bestie] = myFriends;
 //3 copy the 'myFriends' array to a new array called 'myFriendsBackup' using the spread operators
-
+const myFriendsBackup = [...myFriends];
 //after that uncomment the next lines
-//myFriends.pop()
-//console.log(myFriendsBackup) // you should see your full friends list
+myFriends.pop()
+console.log(myFriendsBackup) // you should see your full friends list
 
 //4. create an arrow function called 'restFunction' that receive unknown numbers of parameters using Rest Parameter
 //& print the value of all of them
-
+let restFunction = (...param) => {
+    for (let i = 0; i < param.length ; i++) {
+        console.log(param[i]);
+    }
+}
 //after that uncomment next lines
-//restFunction(myName, ' is the best ', true, [434234]) //you should see all the parameters on the screen
+restFunction(myName, ' is the best ', true, [434234]) //you should see all the parameters on the screen
 
 //5. create an arrow function called 'protectedSum' that receives 3 parameters
 //the third parameter has default value of callback function (try make it arrow)
 //this function sum the first to parameters & pass them to the callback function
-
+let protectedSum = (para1, para2, callbackF = () => {}) => {
+ let result = para1 = para2;
+ callbackF = result;
+}
 //after that uncomment the next lines
-//protectedSum(19, 39) // you should  see an error on the console
+protectedSum(19, 39) // you should  see an error on the console
 
 /** Thanks you, I hope you had fun */
