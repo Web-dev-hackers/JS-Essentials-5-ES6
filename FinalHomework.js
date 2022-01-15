936958963c68e3408079fb2e2c6fe8602f5a29fa
Bushra <btarbah2000@gmail.com>
2022-01-15 16:12:31 +0200

    idk

--- FinalHomework.js
+++ FinalHomework.js
@@ -2,29 +2,48 @@ console.log("FinalHomework.js file linked, Happy Coding :)");
 /*use ES6 syntax in all the tasks */
 //0.a Declare a variable called 'myName' give it your name
 //0.b create an array called 'marks' that has 10 marks in rang from 0 to 100
-
+let MyName = "Bushra" ;
+var marks = [50,70,71,82,66,92,91,84,95,81]
 //0.c Create array called 'myClass' consisting of strings that represent:
 // -name of females in your class
 // -name of males in your class
 // -name of your class instructor ;)
 
+let females = ["Ghalya","Zahra","Aisha"]
+let males = ["Taha","Muhammed","Osama","Muhammed Z"]
+let instructor = ["Essam"]
+let myClass = [{females},{males},{instructor}]
+
 //1 write a function called 'getMiddleValue' that takes an array as an argument and returns the element that is located in the middle of that array.
-
+let getMiddleValue = (array=[]) => {
+let  mid =array[Math.floor(array.length/2)];
+console.log(mid) 
+      }
+      
 //after that uncomment the next 2 lines
-//getMiddleValue([4,6,64,3,2]) // 64
-//getMiddleValue([4,6, 3, 5, 64,3,2]) // 5
-
+getMiddleValue([4,6,64,3,2]) // 64
+getMiddleValue([4,6, 3, 5, 64,3,2]) // 5
 //2 create a function called 'calculateAverage' that receives an array of numbers as a parameter (give it a default value of empty array to prevent error)
 //it returns the average of numbers inside the array
 
+
+let calculateAverage = (potato, tomato, callback =()=>{console.log("cmon")}) => {
+    let sum= potato + tomato ;  
+    let avg = sum / 2     
+    callback (avg)    
+}
+
 //after that uncomment the next lines
-// calculateAverage() // you shouldn't see an error
-// calculateAverage([5, 15, 10]) // 10
+ calculateAverage() // you shouldn't see an error
+ calculateAverage([5, 15, 10]) // 10
 
 //3 use built in filter function to filter array 'marks' to filter marks more than 50 and store them in a variable called 'goodMarks'
-
+let goodMarks  = marks.filter(function(goodMarks){
+    return goodMarks > 50; 
+} 
+)
 //after that uncomment the next line
-//console.log(goodMarks) you should see an array of marks over than 50
+console.log(goodMarks) //you should see an array of marks over than 50
 
 //4 use spread operator to copy the 'marks' array into a new variable called 'marksBackup'
 
@@ -47,3 +66,5 @@ console.log("FinalHomework.js file linked, Happy Coding :)");
 //const [totalEven, totalOdd] = getTotalEvenOdd([1,19,3, 17,21,7,8,9,10, 15, 13])
 //console.log(totalEven) //18
 //console.log(totalOdd) //105
+
+// NO THANKS </3
